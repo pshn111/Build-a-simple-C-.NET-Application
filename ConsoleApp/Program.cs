f8f23fbab41d65978e3cfaf383862f48f6a8df36
pshn111 <spei@ualberta.ca>
2019-04-23 13:50:32 -0600

    First commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Shuonan";
+
+            //change text color
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            //print info
+            Console.WriteLine("{0}: Version{1} by {2}", appName, appVersion, appAuthor);
+            //reset color
+            Console.ResetColor();
+        }
+    }
+}
